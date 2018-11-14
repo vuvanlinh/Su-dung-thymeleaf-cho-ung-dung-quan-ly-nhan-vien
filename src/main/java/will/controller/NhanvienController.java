b54e58d61a41bdeddf00519c9c231984f1382eb2
vuvanlinh95 <vuvanlinh95@gmail.com>
2018-11-14 22:49:07 +0700

    tested

--- src/main/java/will/controller/NhanvienController.java
+++ src/main/java/will/controller/NhanvienController.java
@@ -11,6 +11,8 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import java.util.regex.Matcher;
+
 @Controller
 public class NhanvienController {
     private NhanvienService nhanvienService = new NhanvienServiceImpl();
@@ -21,4 +23,48 @@ public class NhanvienController {
         return "index";
     }
 
+    @GetMapping("/nhanvien/taonhanvienmoi")
+    public String create(Model model) {
+        model.addAttribute("nhanvien", new Nhanvien());
+        return "create";
+    }
+
+    @PostMapping("/nhanvien/luu")
+    public String luu(Nhanvien nhanvien, RedirectAttributes redirectAttributes) {
+        nhanvien.setId((int) (Math.random() * 10000));
+        nhanvienService.save(nhanvien);
+        redirectAttributes.addFlashAttribute("thanhcong", "Luu nhan vien moi thanh cong");
+        return "redirect:/";
+    }
+
+    @GetMapping("/nhanvien/{id}/Chinhsua")
+    public String edit(@PathVariable int id, Model model) {
+        model.addAttribute("nhanvien", nhanvienService.findById(id));
+        return "edit";
+    }
+
+    @PostMapping("/nhanvien/capnhat")
+    public String capnhat(Nhanvien nhanvien, RedirectAttributes redirectAttributes) {
+        nhanvienService.update(nhanvien.getId(), nhanvien);
+        redirectAttributes.addFlashAttribute("thanh cong", "Chinh sua thanh cong");
+        return "redirect:/";
+    }
+
+    @GetMapping("/nhanvien/{id}/Xoa")
+    public String xoa(@PathVariable int id, Model model) {
+        model.addAttribute("nhanvien", nhanvienService.findById(id));
+        return "delete";
+    }
+
+    @PostMapping("/nhanvien/Xoa")
+    public String xoa(Nhanvien nhanvien) {
+        nhanvienService.remove(nhanvien.getId());
+        return "redirect:/";
+    }
+
+    @GetMapping("/nhanvien/{id}/Xem")
+    public String view(@PathVariable int id, Model model) {
+        model.addAttribute("nhanvien", nhanvienService.findById(id));
+        return "view";
+    }
 }
